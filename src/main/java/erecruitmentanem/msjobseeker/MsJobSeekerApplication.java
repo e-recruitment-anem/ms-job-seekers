374306037b7589af9f0b03295723266cf88c9c93
aymenzitouni <a.zitouni@esi-sba.dz>
2022-05-06 10:32:04 +0100

    init project structure

---
+++ src/main/java/erecruitmentanem/msjobseeker/MsJobSeekerApplication.java
@@ -0,0 +1,13 @@
+package erecruitmentanem.msjobseeker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsJobSeekerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsJobSeekerApplication.class, args);
+	}
+
+}
