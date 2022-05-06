6ec6ebe0243e375b8698081af1bee8b65c46e2d1
aymenzitouni <a.zitouni@esi-sba.dz>
2022-05-06 10:37:21 +0100

    add ms configs

---
+++ src/test/java/erecruitmentanem/msjobseeker/MsJobSeekerApplicationTests.java
@@ -0,0 +1,13 @@
+package erecruitmentanem.msjobseeker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsJobSeekerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
